ea694b4b2b7cf351c4c02de505b6c23607c6ec10
Adrian <adrian@theround.com>
2025-07-05 18:31:27 +0100

    routing setup

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Redirect, Slot } from "expo-router";
+import React from "react";
+
+export default function _Layout() {
+  const isAuthenticated = false;
+
+  if (!isAuthenticated) {
+    return <Redirect href="/sign-in" />;
+  }
+
+  return <Slot />;
+}
