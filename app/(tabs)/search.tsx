d2093fa56b7aab8f62b2432c19a6020df38a2d0d
Adrian <adrian@theround.com>
2025-07-23 14:39:55 +0100

    implement search screen

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,16 +1,97 @@
-import seed, { seedWithoutImages } from "@/lib/seed";
-import React from "react";
-import { Button, Text, View } from "react-native";
+/* eslint-disable react-hooks/exhaustive-deps */
+import CartButton from "@/components/CartButton";
+import { getCategories, getMenu } from "@/lib/appwrite";
+import useAppwrite from "@/lib/hooks/useAppwrite";
+import cn from "clsx";
+import { useLocalSearchParams } from "expo-router";
+import React, { useEffect } from "react";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Search = () => {
-  return (
-    <SafeAreaView style={{ padding: 20 }}>
-      <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 20 }}>
-        Search
-      </Text>
+  const { category, query } = useLocalSearchParams<{
+    query: string;
+    category: string;
+  }>();
 
-      <View style={{ gap: 10 }}>
+  const { data, refetch, loading } = useAppwrite({
+    fn: getMenu,
+    params: {
+      category,
+      query,
+      limit: 6,
+    },
+  });
+
+  const { data: categories } = useAppwrite({
+    fn: getCategories,
+  });
+
+  useEffect(() => {
+    refetch({
+      category,
+      query,
+      limit: 6,
+    });
+  }, [category, query]);
+
+  // console.log("DATA", JSON.stringify(data, null, 2));
+
+  return (
+    <SafeAreaView className="h-full bg-white">
+      <FlatList
+        data={data}
+        renderItem={({ item, index }) => {
+          const isFirstRightColItem: boolean = index % 2 === 0;
+          return (
+            <View
+              className={cn(
+                "flex-1 max-w-[48%]",
+                !isFirstRightColItem && "mt-10"
+              )}
+            >
+              <Text>Menu Card</Text>
+            </View>
+          );
+        }}
+        keyExtractor={(item) => item.$id}
+        numColumns={2}
+        columnWrapperStyle={{ gap: 7 }}
+        contentContainerClassName="gap-7 px-5 pb-32"
+        ListHeaderComponent={() => (
+          <View className="my-5 gap-5">
+            <View className="flex-between flex-row w-full">
+              <View className="flex-start">
+                <Text className="small-bold uppercase text-primary">
+                  Search
+                </Text>
+                <View className="flex-start flex-row gap-x-1 mt-0 5">
+                  <Text className="paragraph semi-bold text-dark 100">
+                    Find your favourite food
+                  </Text>
+                </View>
+              </View>
+
+              <CartButton />
+            </View>
+
+            <Text>Search Input</Text>
+
+            <Text>Filter</Text>
+          </View>
+        )}
+        ListEmptyComponent={() =>
+          !loading && (
+            <View className="flex-1 justify-center items-center">
+              <Text className="paragraph semi-bold text-dark 100">
+                No results found
+              </Text>
+            </View>
+          )
+        }
+      />
+
+      {/* <View style={{ gap: 10 }}>
         <Button
           title="Seed Database (with images)"
           onPress={() =>
@@ -28,7 +109,7 @@ const Search = () => {
             )
           }
         />
-      </View>
+      </View> */}
     </SafeAreaView>
   );
 };
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { CreateUserParams, SignInParams } from "@/type";
+import { CreateUserParams, GetMenuParams, SignInParams } from "@/type";
 import { Account, Client, Databases, ID, Query, Storage } from "react-native-appwrite";
 
 export const appwriteConfig = {
@@ -86,6 +86,38 @@ export const getCurrentUser = async () => {
 		if (!currentUser.documents[0]) throw new Error("Failed to get current user");
 
 		return currentUser.documents[0];
+	} catch (e) {
+		throw new Error(e as string)
+	}
+}
+
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+	try {
+		const queries: string[] = [];
+
+		if (category) queries.push(Query.equal("categories", category));
+		if (query) queries.push(Query.search("name", query));
+
+		const menus = await databases.listDocuments(
+			appwriteConfig.databaseId,
+			appwriteConfig.menuCollectionId,
+			queries
+		);
+
+		return menus.documents;
+	} catch (e) {
+		throw new Error(e as string)
+	}
+}
+
+export const getCategories = async () => {
+	try {
+		const categories = await databases.listDocuments(
+			appwriteConfig.databaseId,
+			appwriteConfig.categoriesCollectionId,
+		);
+
+		return categories.documents;
 	} catch (e) {
 		throw new Error(e as string)
 	}
