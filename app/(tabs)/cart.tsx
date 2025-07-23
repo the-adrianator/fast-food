e9a37554d5780926a923ff42c4d5fb103e29bc3a
Adrian <adrian@theround.com>
2025-07-23 18:00:23 +0100

    implement cart store

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,11 +1,90 @@
+import CartItem from "@/components/CartItem";
+import CustomButton from "@/components/CustomButton";
+import CustomHeader from "@/components/CustomHeader";
+import { useCartStore } from "@/store/cart.store";
+import { PaymentInfoStripeProps } from "@/type";
+import cn from "clsx";
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, SafeAreaView, Text, View } from "react-native";
+
+const PaymentInfoStripe = ({
+  label,
+  value,
+  labelStyle,
+  valueStyle,
+}: PaymentInfoStripeProps) => (
+  <View className="flex-between flex-row my-1">
+    <Text className={cn("paragraph-medium text-gray-200", labelStyle)}>
+      {label}
+    </Text>
+    <Text className={cn("paragraph-bold text-dark-100", valueStyle)}>
+      {value}
+    </Text>
+  </View>
+);
 
 const Cart = () => {
+  const { items, getTotalItems, getTotalPrice } = useCartStore();
+
+  const totalItems = getTotalItems();
+  const totalPrice = getTotalPrice();
+
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full pt-5">
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartItem item={item} />}
+        keyExtractor={(item) => item.id}
+        contentContainerClassName="pb-28 px-5 pt-5"
+        ListHeaderComponent={() => <CustomHeader title="Your Cart" />}
+        ListEmptyComponent={() => <Text>No items in cart</Text>}
+        ListFooterComponent={() =>
+          totalItems > 0 && (
+            <View className="gap-5">
+              <View className="mt-6 border border-gray-200 p-5 rounded-2xl">
+                <Text className="h3-bold text-dark-100 mb-5">
+                  Order Summary
+                </Text>
+                <PaymentInfoStripe
+                  label={`Total Items: ${totalItems}`}
+                  value={`£${totalPrice.toFixed(2)}`}
+                  labelStyle="text-gray-200"
+                  valueStyle="text-dark-100"
+                />
+                <PaymentInfoStripe
+                  label="Delivery Fee"
+                  value="£5.00"
+                  labelStyle="text-gray-200"
+                  valueStyle="text-dark-100"
+                />
+                <PaymentInfoStripe
+                  label="Discount"
+                  value={`- £${(totalPrice * 0.1).toFixed(2)}`}
+                  labelStyle="text-gray-200"
+                  valueStyle="!text-success"
+                />
+
+                <View className="border-t border-gray-300 my-2" />
+
+                <PaymentInfoStripe
+                  label="Total"
+                  value={`£${(totalPrice + 5 - totalPrice * 0.1).toFixed(2)}`}
+                  labelStyle="base-bold !text-dark-200"
+                  valueStyle="h3-bold !text-dark-100 !text-right"
+                />
+              </View>
+
+              <CustomButton
+                title="Order Now"
+                style="w-full"
+                textStyle="!text-white"
+                // onPress={() => router.push("/checkout")}
+              />
+            </View>
+          )
+        }
+      />
+    </SafeAreaView>
   );
 };
 
